7c13e54feb3e13c78ddcc83616d06b3564c5fb93
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-21 12:52:23 -0300

    Solution day 2 part two

--- src/main/java/_2015/Day02.java
+++ src/main/java/_2015/Day02.java
@@ -12,8 +12,8 @@ public class Day02 extends Day{
 
         return (2*l*w) + (2*w*h) + (2*h*l) + Collections.min(sides);
     }
-
-    public int getWrappingPeper(){
+    //part one
+    public int getWrappingPaper(){
         List<Integer> paperFoot = new ArrayList<>();
 
         for (String dimension : this.readsInput()){
@@ -29,8 +29,32 @@ public class Day02 extends Day{
 
         return paperFoot.stream().mapToInt(Integer::intValue).sum();
     }
+    //part two
+    public int getRibbon() {
+        return this.readsInput().stream()
+                .mapToInt(dim -> {
+                    String[] p = dim.split("x");
+                    int l = Integer.parseInt(p[0]);
+                    int w = Integer.parseInt(p[1]);
+                    int h = Integer.parseInt(p[2]);
+
+                    int volume = l*w*h;
+
+                    Integer[] sides = {l,w,h};
+
+                    Arrays.sort(sides);
+
+                    int shortestSide1 = sides[0];
+                    int shortestSide2 = sides[1];
+
+                    int sum = 2*(shortestSide1+shortestSide2) + volume;
+
+                    return sum;
+                })
+                .sum();
+    }
 
     public static void main(String[] args) {
-        System.out.println(new Day02().getWrappingPeper());
+        System.out.println(new Day02().getRibbon());
     }
 }
