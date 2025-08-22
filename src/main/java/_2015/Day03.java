546c1a6918f7edf57a92490b161b67ffcc57208b
jot4sz <joaocenaqueleite@hotmail.com>
2025-08-22 18:49:38 -0300

    Solutions day 3 part two

--- src/main/java/_2015/Day03.java
+++ src/main/java/_2015/Day03.java
@@ -6,6 +6,56 @@ import java.util.HashSet;
 
 public class Day03 extends Day {
 
+    public Integer presentsDeliveredWithRobo(){
+        String line = this.readsInput().getFirst();
+        HashSet<String> delivered = new HashSet<>();
+
+        int xRobo = 0;
+        int yRobo = 0;
+
+        int xSanta = 0;
+        int ySanta = 0;
+
+        delivered.add(xSanta +", "+ ySanta);
+
+        for (int i = 0; i < line.length(); i++) {
+            if(i % 2 == 0){
+                switch (line.charAt(i)){
+                    case '>':
+                        xSanta++;
+                        break;
+                    case '<':
+                        xSanta--;
+                        break;
+                    case '^':
+                        ySanta++;
+                        break;
+                    case 'v':
+                        ySanta--;
+                        break;
+                }
+                delivered.add(xSanta +", "+ ySanta);
+            } else {
+                switch (line.charAt(i)){
+                    case '>':
+                        xRobo++;
+                        break;
+                    case '<':
+                        xRobo--;
+                        break;
+                    case '^':
+                        yRobo++;
+                        break;
+                    case 'v':
+                        yRobo--;
+                        break;
+                }
+                delivered.add(xRobo +", "+ yRobo);
+            }
+        }
+        return delivered.size();
+    }
+
     public Integer presentsDelivered(){
         String line = this.readsInput().getFirst();
         HashSet<String> delivered = new HashSet<>();
@@ -39,7 +89,7 @@ public class Day03 extends Day {
 
 
     public static void main(String[] args) {
-        System.out.println(new Day03().presentsDelivered());
+        System.out.println(new Day03().presentsDeliveredWithRobo());
 
     }
 }
